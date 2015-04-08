32df01f0eb980934e6cb248ff1de4e76ea5b95e5
Allexit <horde98@gmail.com>
2015-04-08 16:59:01 +0300

    initial commit

---
+++ desktop/src/com/saltosion/gladiator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.saltosion.gladiator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.saltosion.gladiator.GladiatorBrawler;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GladiatorBrawler(), config);
+	}
+}
